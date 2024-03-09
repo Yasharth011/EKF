1edf23c7cbdb5847dc8a582138ad5c923a7aa95f
Yasharth011 <yasharthgupta10@gmail.com>
2024-03-09 18:54:04 +0530

    Intial commit

---
+++ LPF.py
@@ -0,0 +1,8 @@
+from scipy import butter, filtfilt
+
+def butter_lowpass_filter(sig, cutoff, fs, order):
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a, data)
+    return y
