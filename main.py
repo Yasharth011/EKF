1ab115715774ff6219239116e5de025ad0a433eb
Yasharth011 <yasharthgupta10@gmail.com>
2024-03-17 18:48:50 +0530

    subscribed to gazebo imu

--- main.py
+++ main.py
@@ -3,15 +3,9 @@ import math
 import numpy as np
 import plot
 import matplotlib.pyplot as plt
-import pyrealsense2 as rs
-
-pipeline = rs.pipeline()
-config = rs.config()
-config.enable_stream(rs.stream.accel, rs.format.motion_xyz32f, 200)
-config.enable_stream(rs.stream.gyro, rs.format.motion_xyz32f, 200)
-
-pipeline.start(config)
-
+from gz.msgs10.vector3d_pb2 import Vector3d
+from gz.transport13 import Node
+from gz.msgs10.imu_pb2 import IMU
 Q = np.diag([.1, #var(x)
               .1, #var(y)
               np.deg2rad(1), #var(yaw)
@@ -31,7 +25,27 @@ dt = 0.1 # time-step
 
 show_animation = True
 
+accel_net = 0.0
 
+gyro = 0
+
+u = np.zeros((2,1))
+
+def vector3_cb(msg):
+    msg1 = msg.linear_acceleration
+    global accel
+    accel = np.array([msg1.x, msg1.y, msg1.z])
+
+    msg2 = msg.angular_velocity
+    global gyro
+    gyro =  msg2.y
+    
+    global accel_net
+    accel_net = math.sqrt((accel[0]**2)+(accel[1]**2)+(accel[2]**2))
+	
+    global u
+    u = np.array([[accel_net*dt], [gyro]])
+    
 def observation(xTrue, u):
     xTrue = state_model(xTrue, u)
 
@@ -101,6 +115,8 @@ def main():
 
     time = 0.0
 
+    node = Node()
+
     #state vector 
     xEst = np.zeros((4,1))
     xTrue = np.zeros((4,1))
@@ -110,26 +126,15 @@ def main():
     hxEst = xEst
     hxTrue = xTrue 
 
+    if node.subscribe(IMU, "/world/default/model/rover/link/base_link/sensor/imu_sensor/imu", vector3_cb):
+       	print("Subscribing to type {} on topic [{}]".format(Vector3d, "IMU data"))
+        
+    else:
+       	print("Error subscribing to topic [{}]".format("IMU data"))
+       	return
+ 
     while True:
 
-        frames = pipeline.wait_for_frames()
-
-        raw_accel = frames[0].as_motion_frame().get_motion_data()
-        raw_gyro = frames[1].as_motion_frame().get_motion_data()
-
-        rs_to_base_tfm = np.asarray([[0,0,1],[1,0,0],[0,1,0]])#transfromation matrix
-
-        accel = np.asarray([raw_accel.x, raw_accel.y, raw_accel.z])
-        accel = np.transpose(np.matmul(rs_to_base_tfm, np.transpose(accel)))
-
-        gyro = np.asarray([raw_gyro.x, raw_gyro.y, raw_gyro.z])
-        gyro = np.transpose(np.matmul(rs_to_base_tfm, np.transpose(gyro)))
-
-        #calculating net acceleration
-        accel_net = math.sqrt((pow(accel[0],2) + pow(accel[1],2)))
-
-        u = np.array([[accel_net*dt], [gyro[2]]]) #control input
-        
         time+= dt
 
         xTrue, ud = observation(xTrue, u)
@@ -141,7 +146,7 @@ def main():
         #store data histroy 
         hxEst = np.hstack((hxEst, xEst))
         hxTrue = np.hstack((hxTrue, xTrue))
-        
+        print(u) 
         if show_animation:
             plt.cla()
             
